cd6c3939f1bac42ffecbd06318d23f71d42219f8
Justin Campana <justin.campana@gmail.com>
2016-11-01 20:26:41 -0700

    Various updates

--- globals.py
+++ globals.py
@@ -1,8 +1,7 @@
-# quandl_symbols = ["CHRIS/CME_ES1", "CHRIS/CME_GC1", "CHRIS/CME_SI1", "CHRIS/CME_CL1", "CHRIS/CME_TY1", "CHRIS/ICE_DX1"
-#                     , "CHRIS/CME_C1", "CHRIS/CME_S1", "CHRIS/CME_JY1", "CHRIS/CME_BP1", "CHRIS/CME_EC1"
-#                     , "CHRIS/CME_AD1", "CHRIS/CME_NE1", "CHRIS/CME_SF1"]
+quandl_symbols = ["CHRIS/CME_ES1", "CHRIS/CME_GC1", "CHRIS/CME_SI1", "CHRIS/CME_CL1", "CHRIS/CME_TY1", "CHRIS/ICE_DX1"
+                    , "CHRIS/CME_C1", "CHRIS/CME_S1", "CHRIS/CME_JY1", "CHRIS/CME_BP1", "CHRIS/CME_EC1"
+                    , "CHRIS/CME_AD1", "CHRIS/CME_NE1", "CHRIS/CME_SF1"]
 
-quandl_symbols = ["CHRIS/CME_SI1"]
 quandl_api_key = 'gzBJon8Nc6pbn6WytL_H'
 quandl_start_date = '1900-01-01'
 quandl_end_date = '2016-12-31'
