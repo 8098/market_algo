c9a9a24afc199b027be38d87f039f096c5fc7331
Justin Campana <justin.campana@gmail.com>
2016-11-06 20:23:38 -0800

    Various updates

--- get_technicals.py
+++ get_technicals.py
@@ -6,7 +6,7 @@ import talib as ta
 
 
 sql_connection = psycopg2.connect(host=host, port=port, user=user, database=database)
-sql = "SELECT timestamp, close FROM foreximport WHERE symbol = 'usdjpy';"
+sql = "SELECT timestamp, close FROM foreximport WHERE symbol = 'eurusd';"
 data_frame = pd.read_sql(sql, sql_connection)
 
 analysis = pd.DataFrame
@@ -15,4 +15,6 @@ data_frame['rsi'] = ta.RSI(np.array(data_frame.close), timeperiod=10)
 data_frame['slow_rsi'] = ta.EMA(np.array(data_frame.rsi), timeperiod=10)
 data_frame['roc'] = ta.ROC(np.array(data_frame.close), timeperiod=10)
 data_frame['slow_roc'] = ta.EMA(np.array(data_frame.roc), timeperiod=10)
+data_frame['mama'], data_frame['fama'] = ta.MAMA(np.array(data_frame.close), fastlimit=0.5, slowlimit=0.05)
+data_frame['ht_trend'] = ta.HT_TRENDLINE(np.array(data_frame.close))
 print(data_frame)
