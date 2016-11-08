8d1133042dd8396b77499c800d3759aa75bf9975
Justin Campana <justin.campana@gmail.com>
2016-11-07 20:14:35 -0800

    Various updates

--- get_technicals.py
+++ get_technicals.py
@@ -4,9 +4,10 @@ import pandas as pd
 import numpy as np
 import talib as ta
 import file_iterator as fi
+import datetime
 
 
-table_name = 'forexanalysis2'
+table_name = 'forexanalysis1'
 
 
 def create_table(table):
@@ -39,7 +40,7 @@ def create_table(table):
 def get_data():
     print("Getting raw data for foreximport")
     sql_connection = psycopg2.connect(host=host, port=port, user=user, database=database)
-    sql = "SELECT * FROM foreximport WHERE symbol = 'eurusd';"
+    sql = "SELECT * FROM foreximport;"
     data_frame = pd.read_sql(sql, sql_connection)
 
     print("Getting technicals")
@@ -64,28 +65,6 @@ def insert_db(df, table):
     series = []
 
     for row in data_dict.values():
-        # cursor.execute(
-        #     """INSERT INTO """ + table + """ VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-        #     , %s, %s, %s, %s, %s);"""
-        #     , (row['id']
-        #     , row['symbol']
-        #     , row['timestamp']
-        #     , row['date']
-        #     , row['time']
-        #     , row['open']
-        #     , row['high']
-        #     , row['low']
-        #     , row['close']
-        #     , row['import']
-        #     , row['ema'] if "nan" not in str(row['ema']) else 0
-        #     , row['rsi'] if "nan" not in str(row['rsi']) else 0
-        #     , row['slow_rsi'] if "nan" not in str(row['slow_rsi']) else 0
-        #     , row['roc'] if "nan" not in str(row['roc']) else 0
-        #     , row['slow_roc'] if "nan" not in str(row['slow_roc']) else 0
-        #     , row['mama'] if "nan" not in str(row['mama']) else 0
-        #     , row['fama'] if "nan" not in str(row['fama']) else 0
-        #     , row['ht_trend'] if "nan" not in str(row['ht_trend']) else 0))
-
         series.append(((row['id']
            , row['symbol']
            , row['timestamp']
@@ -106,16 +85,10 @@ def insert_db(df, table):
            , row['ht_trend'] if "nan" not in str(row['ht_trend']) else 0)))
 
     series.sort()
-    # print(series[0])
-    # records_list_template = ','.join(['%s'] * len(series))
-    # insert_query = 'INSERT INTO ' + table + ' VALUES {0}'.format(records_list_template)
-    # cursor.execute(insert_query, series)
-    f = fi.IteratorFile(("{}\t{}".format(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]
-                                         , x[8], x[9], x[10], x[11], x[12], x[13], x[14]
-                                         , x[15], x[16], x[17]) for x in series))
-    cursor.copy_from(f, table, columns=('id', 'symbol', 'timestamp', 'date', 'time', 'open'
-                                        , 'high', 'low', 'close', 'import', 'ema', 'rsi'
-                                        ,'slow_rsi', 'roc', 'slow_roc', 'mama', 'fama', 'ht_trend'))
+    f = fi.IteratorFile(("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}"
+        .format(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13]
+        , x[14], x[15], x[16], x[17]) for x in series))
+    cursor.copy_from(f, table)
     sql_connection.commit()
     cursor.close()
     sql_connection.close()
@@ -123,8 +96,15 @@ def insert_db(df, table):
 
 
 def main():
-    # create_table(table_name)
+    start_time = datetime.datetime.now()
+    print("Started at " + str(start_time))
+
+    create_table(table_name)
     df = get_data()
     insert_db(df, table_name)
+
+    end_time = datetime.datetime.now()
+    duration = (end_time - start_time).days * 24 * 60
+    print("Finished in " + str(duration) + " minutes")
 
 main()
