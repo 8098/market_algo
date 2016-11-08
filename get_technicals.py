0de7ef6bb8ca6c8add9480ac1138369f5b6c45ac
slampana <justin.campana@gmail.com>
2016-11-07 16:29:07 -0800

    Various updates

--- get_technicals.py
+++ get_technicals.py
@@ -3,9 +3,10 @@ import psycopg2
 import pandas as pd
 import numpy as np
 import talib as ta
+import file_iterator as fi
 
 
-table_name = 'forexanalysis1'
+table_name = 'forexanalysis2'
 
 
 def create_table(table):
@@ -63,28 +64,58 @@ def insert_db(df, table):
     series = []
 
     for row in data_dict.values():
-        cursor.execute(
-            """INSERT INTO """ + table + """ VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-            , %s, %s, %s, %s, %s);"""
-            , (row['id']
-            , row['symbol']
-            , row['timestamp']
-            , row['date']
-            , row['time']
-            , row['open']
-            , row['high']
-            , row['low']
-            , row['close']
-            , row['import']
-            , row['ema'] if "nan" not in str(row['ema']) else 0
-            , row['rsi'] if "nan" not in str(row['rsi']) else 0
-            , row['slow_rsi'] if "nan" not in str(row['slow_rsi']) else 0
-            , row['roc'] if "nan" not in str(row['roc']) else 0
-            , row['slow_roc'] if "nan" not in str(row['slow_roc']) else 0
-            , row['mama'] if "nan" not in str(row['mama']) else 0
-            , row['fama'] if "nan" not in str(row['fama']) else 0
-            , row['ht_trend'] if "nan" not in str(row['ht_trend']) else 0))
+        # cursor.execute(
+        #     """INSERT INTO """ + table + """ VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+        #     , %s, %s, %s, %s, %s);"""
+        #     , (row['id']
+        #     , row['symbol']
+        #     , row['timestamp']
+        #     , row['date']
+        #     , row['time']
+        #     , row['open']
+        #     , row['high']
+        #     , row['low']
+        #     , row['close']
+        #     , row['import']
+        #     , row['ema'] if "nan" not in str(row['ema']) else 0
+        #     , row['rsi'] if "nan" not in str(row['rsi']) else 0
+        #     , row['slow_rsi'] if "nan" not in str(row['slow_rsi']) else 0
+        #     , row['roc'] if "nan" not in str(row['roc']) else 0
+        #     , row['slow_roc'] if "nan" not in str(row['slow_roc']) else 0
+        #     , row['mama'] if "nan" not in str(row['mama']) else 0
+        #     , row['fama'] if "nan" not in str(row['fama']) else 0
+        #     , row['ht_trend'] if "nan" not in str(row['ht_trend']) else 0))
 
+        series.append(((row['id']
+           , row['symbol']
+           , row['timestamp']
+           , row['date']
+           , row['time']
+           , row['open']
+           , row['high']
+           , row['low']
+           , row['close']
+           , row['import']
+           , row['ema'] if "nan" not in str(row['ema']) else 0
+           , row['rsi'] if "nan" not in str(row['rsi']) else 0
+           , row['slow_rsi'] if "nan" not in str(row['slow_rsi']) else 0
+           , row['roc'] if "nan" not in str(row['roc']) else 0
+           , row['slow_roc'] if "nan" not in str(row['slow_roc']) else 0
+           , row['mama'] if "nan" not in str(row['mama']) else 0
+           , row['fama'] if "nan" not in str(row['fama']) else 0
+           , row['ht_trend'] if "nan" not in str(row['ht_trend']) else 0)))
+
+    series.sort()
+    # print(series[0])
+    # records_list_template = ','.join(['%s'] * len(series))
+    # insert_query = 'INSERT INTO ' + table + ' VALUES {0}'.format(records_list_template)
+    # cursor.execute(insert_query, series)
+    f = fi.IteratorFile(("{}\t{}".format(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7]
+                                         , x[8], x[9], x[10], x[11], x[12], x[13], x[14]
+                                         , x[15], x[16], x[17]) for x in series))
+    cursor.copy_from(f, table, columns=('id', 'symbol', 'timestamp', 'date', 'time', 'open'
+                                        , 'high', 'low', 'close', 'import', 'ema', 'rsi'
+                                        ,'slow_rsi', 'roc', 'slow_roc', 'mama', 'fama', 'ht_trend'))
     sql_connection.commit()
     cursor.close()
     sql_connection.close()
